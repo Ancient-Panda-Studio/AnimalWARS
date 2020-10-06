51c34b4c51be12b65839e0057a3791002d95d9d8
Oriol Pujol <61847424+kedalen12@users.noreply.github.com>
2020-10-06 18:48:52 +0200

    Test

--- GameClient/Assets/Scripts/Network/Constants.cs
+++ GameClient/Assets/Scripts/Network/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static readonly string WebServer = "http://localhost/sqlconnect/";
     public static string Inviter;
-    public static string filePath = @"C:\Users\Kedalen\Documents\AnimalWars\Settings\Settings.xml";
+    public static string filePath = @"C:\Users\oriol\Documents\AnimalWars\Settings\Settings.xml";
     public static int ID { get; set; }
     public static string Username { get; set; }
     public static int Score { get; set; }
