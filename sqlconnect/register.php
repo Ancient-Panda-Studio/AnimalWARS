fd79cd13ae6be41f0bfc36c57e1c4a06517dacf2
kedalen <kedalen@localhost.localdomain>
2020-10-19 11:32:46 +0200

    SQL-CONNECT-UPDATE

--- sqlconnect/register.php
+++ sqlconnect/register.php
@@ -8,6 +8,7 @@
 
 	$username = $_POST["user"];
 	$password = $_POST["pass"];
+    $email = $_POST["email"];
 
 	//Check if name exists
 
@@ -17,13 +18,13 @@
 		echo "Error code #3 USERNAME IS TAKEN"; //Error code #3 = Username already exists
 		exit();
 	}
-
 	//Add user to table
  	$salt = "\$5\$rounds=5000\$" . "steamedhams" . $username . "\$";
  	$hash = crypt($password, $salt);
- 	$insertuserquerry = "INSERT INTO users (username, hash, salt) VALUES ('" . $username . "', '" . $hash . "', '" . $salt . "');";
- 	mysqli_query($connection, $insertuserquerry) or die("Error code #4 INSERTION TO PLAYER INTO THE DATABASE FAILED"); //Error code #4 = Insert user failed
-
+    $insertuserquerry = "INSERT INTO users (username, hash, salt, email) VALUES ('" . $username . "', '" . $hash . "', '" . $salt . "', '" . $email . "');";
+    mysqli_query($connection, $insertuserquerry) or die("Error code #4 INSERTION TO PLAYER INTO THE DATABASE FAILED"); //Error code #4 = Insert user failed
+    $sqlInsert = "INSERT INTO userdata (username) VALUE ('" . $username . "');" or die("kekw ERROR");
+    mysqli_query($connection, $sqlInsert) or die ("kekw");
  	echo ("0");
 	$connection->close();
 ?>
