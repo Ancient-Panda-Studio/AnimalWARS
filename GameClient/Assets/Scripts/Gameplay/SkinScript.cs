035ab784af260b5e866d8805275604ad0c590867
Oriol Pujol <61847424+kedalen12@users.noreply.github.com>
2021-02-17 02:32:46 +0000

    GitReset

--- GameClient/Assets/Scripts/Gameplay/SkinScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public class SkinScript : MonoBehaviour
-{
-    public Text price;
-    public Text skinName;
-    public Image sprite;
-}
--- sqlconnect/constant.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_USER", "root");
-define("SERVER_NAME", "localhost");
-define("DB_PASSWORD", "");
-define("DB_NAME", "unityaccess");
-
-$CON = mysqli_connect(SERVER_NAME, DB_USER, DB_PASSWORD, DB_NAME);
